54cf41bbd2b8ea82b381940c5e53d8919b27e11b
claudiozh <claudiorodrigozh@gmail.com>
2024-02-08 17:21:52 -0300

    feat: add bank configuration and routes to register polls and votes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,20 @@
 import fastify from "fastify";
+import cookie from '@fastify/cookie'
+
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: '68cbf17441e1f4329deba802e25989f4',
+  hook: 'onRequest',
+})
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
