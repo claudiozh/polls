ec176cfae2ad19e227f690d2cf269b3467c7790f
claudiozh <claudiorodrigozh@gmail.com>
2024-02-09 16:17:32 -0300

    feat: add websockets, redis and endpoint to vote on poll

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify"
 import z from "zod"
 import prisma from "../../lib/prisma"
+import { redis } from "../../lib/redis"
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -23,7 +24,37 @@ export async function getPoll(app: FastifyInstance) {
         }
       }
     })
+
+    if(!poll) {
+      return reply.status(404).send({
+        message: 'Poll not found'
+      })
+    }
+
+    const INITIAL_OPTION = 0
+    const ALL_OPTIONS = -1
+    const result = await redis.zrange(pollId, INITIAL_OPTION, ALL_OPTIONS, 'WITHSCORES')
+
+    const votes = result.reduce((acc, item, index) => {
+      if(index % 2 === 0) {
+        acc[item] = Number(result[index + 1])
+      }
+
+      return acc
+    }, {} as Record<string, number>)
   
-    return reply.send({ poll })
+    return reply.send({ 
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => {
+          return {
+            id: option.id,
+            title: option.title,
+            votes: votes[option.id] ?? 0
+          }
+        })
+      }
+     })
   })
 }
--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify"
 import { randomUUID } from "node:crypto"
 import z from "zod"
 import prisma from "../../lib/prisma"
+import { redis } from "../../lib/redis"
+import { votingPubSub } from "../../utils/voting-pub-sub"
 
 
 export async function voteOnPoll(app: FastifyInstance) {
@@ -36,6 +38,13 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id
           }
         })
+
+        const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+        votingPubSub.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes)
+        })
       }
       else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({
@@ -61,6 +70,13 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollId,
         pollOptionId
       }
+    })
+
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    votingPubSub.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
     })
 
     return reply.status(201).send()
